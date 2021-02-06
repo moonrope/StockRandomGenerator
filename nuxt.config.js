8b49479ea3b73c1556873d0ec238973b50e91cfc
Miguel Angel Aguirre <miguel@moonrope.com>
2021-02-06 12:33:38 +0200

    config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,7 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'server',
-  generate: {
-    dir: 'public'
-  },
+  target: 'static',
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'brainstock',
