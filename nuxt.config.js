8b8fb2e73a0d66e6de6589e3d54e95f7ba32ae41
Miguel Angel Aguirre <miguel@moonrope.com>
2021-02-06 13:05:46 +0200

    Change updates

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,13 +45,5 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    extend ( config, { isDev, isClient, isServer } ) {
-      if ( isServer ) {
-        config.externals = {
-          '@vue-chartjs/line':'commonjs @vue-chartjs/line',
-          '@vue-chartjs/mixins':'commonjs @vue-chartjs/mixins',
-        }
-      }
-    }
   }
 }
